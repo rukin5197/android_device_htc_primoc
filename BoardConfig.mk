de9ccf2673e7a9509baf9e7fa8dd910aa47a055b
Gannon Macdonald <gannon050197@gmail.com>
2013-03-30 18:08:14 -0500

    Bluetooth libbt_vndcfg addition and remove goldfish blobs

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -87,3 +87,8 @@ TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/class/android_usb/android0/f_mass_storag
 
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/htc/primoc/bluetooth
+BOARD_BLUEDROID_VENDOR_CONF := device/htc/primoc/configs/libbt_vndcfg.txt
+
+# No SDK blobs
+BUILD_EMULATOR_SENSORS_MODULE := false
+BUILD_EMULATOR_GPS_MODULE := false
